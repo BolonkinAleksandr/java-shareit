4bc14f68ee5673394b66d215f450ea9b82b1e170
Александр <bolonkinalexander@yandex.ru>
2022-12-09 18:54:33 +0300

    add-item-requests

--- src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
