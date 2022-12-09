7ca8eb6864320fd650a614d4195bec8abe684f32
Александр <bolonkinalexander@yandex.ru>
2022-12-09 18:37:14 +0300

    add-item-requests

--- src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 public class ItemRequestRepositoryTest {
